696f1dc4b0c2f9c491e0a4ac1d5131f1321df172
Ferhat Hamza <Ferhathamza17@gmail.com>
2025-10-10 22:20:00 +0100

    Update dashboard.js

--- scripts/dashboard.js
+++ scripts/dashboard.js
@@ -45,7 +45,7 @@ document.addEventListener("DOMContentLoaded", async () => {
         const tr = document.createElement("tr");
         tr.innerHTML = `
           <td>${s.first_name} ${s.last_name}</td>
-          <td class="${s.morning_present ? "present" : "absent"}"><input type="checkbox" class="morning" ${!s.morning_present ? "checked" : ""}></td>
+          <td class="${!s.morning_present ? "present" : "absent"}"><input type="checkbox" class="morning" ${!s.morning_present ? "checked" : ""}></td>
           <td class="${s.evening_present ? "present" : "absent"}"><input type="checkbox" class="evening" ${s.evening_present ? "checked" : ""}></td>
           <td><input type="checkbox" class="english" ${s.english ? "checked" : ""}></td>
           <td><input type="checkbox" class="french" ${s.french ? "checked" : ""}></td>
